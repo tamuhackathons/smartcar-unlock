f28402e0b4c8648770810d1ba6461458a804e6e9
Ruben Vazquez-Chapa <ravc@protonmail.com>
2019-01-26 11:35:56 -0600

    Add permissions

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ client = smartcar.AuthClient(
     client_id="7f76ba71-1fdc-4bc3-92e9-51c7f3304a41",
     client_secret="e1fddb8c-d5cb-4a2a-a9aa-12958b3bc73e",
     redirect_uri="http://localhost:8000/exchange",
-    scope=['read_vehicle_info', "control_security", "control_security:unlock", "control_security:lock"],
+    scope=['read_vehicle_info', "control_security", 
+           "control_security:unlock", "control_security:lock", 
+           "read_location", "read_odometer", "read_vin"],
     test_mode=True,
 )
 
 @app.route('/login', methods=['GET'])
 def login():
-    # TODO: Authorization Step 1b: Launch Smartcar authentication dialog
     auth_url = client.get_auth_url()
     print(auth_url)
     return redirect(auth_url)
@@ -24,7 +25,6 @@ def login():
 
 @app.route('/exchange', methods=['GET'])
 def exchange():
-    # TODO: Authorization Step 3: Handle Smartcar response
     code = request.args.get('code')
     
     print(code)
@@ -33,20 +33,18 @@ def exchange():
     
     access = client.exchange_code(code)
     
-    return '', 200
+    return redirect(f"vehicle/{0}")
 
-@app.route('/vehicle', methods=['GET'])
-def vehicle():
-    # TODO: Request Step 2: Get vehicle ids
-    # access our global variable to retrieve our access tokens
+@app.route('/vehicle/<vid>/', methods=['GET'])
+def vehicle(vid):
     global access
-    # the list of vehicle ids
+    
     vehicle_ids = smartcar.get_vehicle_ids(
         access['access_token'])['vehicles']
     
     print(vehicle_ids)
     
-    vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
+    vehicle = smartcar.Vehicle(vehicle_ids[int(vid)], access['access_token'])
     
     info = vehicle.info()
     
@@ -63,7 +61,7 @@ def unlock(vid):
     vehicle = smartcar.Vehicle(vehicle_ids[int(vid)], access['access_token'])
     vehicle.unlock()
     
-    return '', 200
+    return redirect(f"vehicle/{vid}")
 
 @app.route('/vehicle/<vid>/lock')
 def lock(vid):
@@ -74,7 +72,7 @@ def lock(vid):
     vehicle = smartcar.Vehicle(vehicle_ids[int(vid)], access['access_token'])
     vehicle.unlock()
     
-    return '', 200
+    return redirect(f"vehicle/{vid}")
 
 @app.route('/vehicle/<vid>/locate')
 def locate(vid):
@@ -83,6 +81,10 @@ def locate(vid):
     
     
     vehicle = smartcar.Vehicle(vehicle_ids[int(vid)], access['access_token'])
+    
+    location_data = vehicle.location()
+    
+    return jsonify(location_data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port="8000")
