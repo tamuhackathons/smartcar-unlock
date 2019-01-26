6c722c3943662920a8a020c3477b7f46d424b0af
Ruben Vazquez-Chapa <ravc@protonmail.com>
2019-01-26 12:21:19 -0600

    Add user id

--- main.py
+++ main.py
@@ -86,5 +86,9 @@ def refreshtoken():
     
     return redirect(f"vehicle/0")
 
+@app.route('/user')
+def user():
+    return jsonify(smartcar.get_user_id())
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port="8000")
